81ebc3af3abea581cc9cd1b356534fca7bb11f89
Matumba <78918727+Matorota@users.noreply.github.com>
2023-10-10 23:36:22 +0300

    Update 2uz.cpp

--- 2uz/2uz/2uz.cpp
+++ 2uz/2uz/2uz.cpp
@@ -8,89 +8,90 @@ Tam bus reikalinga patobulintas šifravimo/dešifravimo metodas.
 #include <string>
 
 
-//char abc[26] = { 'A','B','C','D','E','F','G','H', 'I','Y','J','K','L','M','N','O','P','R','S','T','U','V','Z' }; // PRIRASYTI
-char abc[32] = { 'A','Ą','B','C','Č','D','E','Ę','Ė','F','G','H', 'I','Į','Y','J','K','L','M','N','O','P','R','S','Š','T','U','Ų','Ū','V','Z','Ž' };
+//char abc[32] = { 'A','Ą','B','C','Č','D','E','Ę','Ė','F','G','H', 'I','Į','Y','J','K','L','M','N','O','P','R','S','Š','T','U','Ų','Ū','V','Z','Ž' };
+char abc[26] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
 using namespace std;
 
-int findIndex(char c, bool useASCII);
+int findIndex(char c);
 string Encrypt(string text, string key, bool useASCII);
-string Decrypt(string text, string key, bool useASCII);
-
+string Decrypt(string encrypted, string key, bool useASCII);
 
 int main() {
-
-    setlocale(LC_ALL, "Lithuanian"); // neveikia
     bool useASCII;
-    string text;
+    string text, key;
     cout << "Norite naudoti ASCII? (1 - taip, 0 - ne): \n";
     cin >> useASCII;
+    cin.ignore();
     cout << "Parasykit zody: \n";
-    cin >> text;
-    string key;
+    getline(cin, text);
     cout << "Parasykit rakta: \n";
-    cin >> key;
+    getline(cin, key);
     string encrypted = Encrypt(text, key, useASCII);
     string decrypted = Decrypt(encrypted, key, useASCII);
     cout << "Pradinis tekstas: " << text << endl;
     cout << "Užšifruotas tekstas: " << encrypted << endl;
     cout << "Dešifruotas tekstas: " << decrypted << endl;
-
     return 0;
 }
 
-int findIndex(char c, bool useASCII) {
-    if (useASCII) return (int)c;
-    for (int i = 0; i < 32; i++) {
+int findIndex(char c) {
+    for (int i = 0; i < 26; i++) {
         if (abc[i] == c) return i;
     }
     return -1;
 }
 
 string Encrypt(string text, string key, bool useASCII = false) {
-    int n = useASCII ? 128 : 32;
-    string encrypted = "";
-    for (int i = 0, j = 0; i < text.length(); i++) {
-        char mi = text[i];
-        char ki = key[j % key.length()];
-
-        int miIndex = findIndex(mi, useASCII);
-        int kiIndex = findIndex(ki, useASCII);
-
-        if (miIndex == -1 || kiIndex == -1) continue;
-
-        if (useASCII) {
-            encrypted += char((miIndex + kiIndex) % n);
+    if (useASCII) {
+        stringstream ss;
+        for (char c : text) {
+            ss << int(c) << " ";
         }
-        else {
-            encrypted += abc[(miIndex + kiIndex) % n];
-        }
-
-        j++;
+        return ss.str();
     }
-    return encrypted;
+    else {
+        string encrypted = "";
+        for (int i = 0, j = 0; i < text.length(); i++) {
+            char mi = text[i];
+            char ki = key[j % key.length()];
+
+            int miIndex = findIndex(mi);
+            int kiIndex = findIndex(ki);
+
+            if (miIndex == -1 || kiIndex == -1) continue;
+            encrypted += abc[(miIndex + kiIndex) % 26];
+
+            j++;
+        }
+        return encrypted;
+    }
 }
 
-string Decrypt(string text, string key, bool useASCII = false) {
-    int n = useASCII ? 128 : 32;
-    string decrypted = "";
-    for (int i = 0, j = 0; i < text.length(); i++) {
-        char ci = text[i];
-        char ki = key[j % key.length()];
-
-        int ciIndex = findIndex(ci, useASCII);
-        int kiIndex = findIndex(ki, useASCII);
-
-        if (ciIndex == -1 || kiIndex == -1) continue;
-
-        if (useASCII) {
-            decrypted += char((ciIndex - kiIndex + n) % n);
+string Decrypt(string encrypted, string key, bool useASCII = false) {
+    if (useASCII) {
+        stringstream ss(encrypted);
+        string decrypted;
+        int num;
+        while (ss >> num) {
+            decrypted += char(num);
         }
-        else {
-            decrypted += abc[(ciIndex - kiIndex + n) % n];
-        }
-
-        j++;
+        return decrypted;
     }
-    return decrypted;
+    else {
+        string decrypted = "";
+        for (int i = 0, j = 0; i < encrypted.length(); i++) {
+            char ci = encrypted[i];
+            char ki = key[j % key.length()];
+
+            int ciIndex = findIndex(ci);
+            int kiIndex = findIndex(ki);
+
+            if (ciIndex == -1 || kiIndex == -1) continue;
+            decrypted += abc[(ciIndex - kiIndex + 26) % 26];
+
+            j++;
+        }
+        return decrypted;
+    }
 }
